9ae37a4e13cf8c71942cfd0d9a00fc684247cd68
Tom <tom.ruijs@student.fontys.nl>
2018-05-28 15:07:07 +0200

    Bats to improve workflow

---
+++ deployrating.bat
@@ -0,0 +1,2 @@
+java -jar payara-micro-5.181.jar --deploy .\RatingService\target\RatingService-1.0-SNAPSHOT.war
+pause 
---
+++ deployreserve.bat
@@ -0,0 +1,2 @@
+java -jar payara-micro-5.181.jar --deploy .\ReserveService\target\ReserveService-1.0-SNAPSHOT.war
+pause 
