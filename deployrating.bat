6fa61f617ada2cfcacb356064dded452401b43a2
Tom <tom.ruijs@student.fontys.nl>
2018-05-28 15:26:47 +0200

    dfasdfdadsf

--- deployrating.bat
+++ deployrating.bat
@@ -1,2 +1,2 @@
-java -jar payara-micro-5.181.jar --deploy .\RatingService\target\RatingService-1.0-SNAPSHOT.war
+java -jar payara-micro-5.181.jar --deploy .\RatingService\target\RatingService-1.0-SNAPSHOT.war --port 15476
 pause 
--- deployreserve.bat
+++ deployreserve.bat
@@ -1,2 +1,2 @@
-java -jar payara-micro-5.181.jar --deploy .\ReserveService\target\ReserveService-1.0-SNAPSHOT.war
+java -jar payara-micro-5.181.jar --deploy .\ReserveService\target\ReserveService-1.0-SNAPSHOT.war --port 15476
 pause 
