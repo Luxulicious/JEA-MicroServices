4e6ed40d989c7f8eef86a6deaa384ff50e41b23d
Roelofs,Tom T <t.roelofs@student.fontys.nl>
2018-05-30 13:10:55 +0200

    app components

--- movie-app/src/app/app.module.ts
+++ movie-app/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { PaymentComponent } from './payment/payment.component';
+import { RatingComponent } from './rating/rating.component';
+import { ReserveComponent } from './reserve/reserve.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaymentComponent,
+    RatingComponent,
+    ReserveComponent
   ],
   imports: [
     BrowserModule
