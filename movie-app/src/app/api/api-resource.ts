402cbdc80919280e5970e72b84f4cfe5cba17fea
Roelofs,Tom T <t.roelofs@student.fontys.nl>
2018-06-04 10:42:13 +0200

    base url payment

--- movie-app/src/app/api/api-resource.ts
+++ movie-app/src/app/api/api-resource.ts
@@ -1,3 +1,3 @@
 export let baseUrlReservations: string = 'http://localhost:1240/ReserveService-1.0-SNAPSHOT/api/reservations/';
 export let baseUrlRatings: string = 'http://localhost:1242/RatingService-1.0-SNAPSHOT/api/';
-export let baseUrl: String = 'http://localhost:8080/PaymentService/api/';
+export let baseUrl: string = 'http://localhost:1241/PaymentService-1.0-SNAPSHOT/api/';
